d54bb211c777c1cdc9dc41816ec241b16816d4f9
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-06-14 19:55:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ async function checkWeather(city) {
 
   switch (weather_data.weather[0].main) {
     case `Clouds`:
-      weather_img.src = "img/cloud.png";
+      weather_img.src = "cloud.png";
       break;
     case `Clear`:
-      weather_img.src = "/img/clear.png";
+      weather_img.src = "clear.png";
       break;
     case `Rain`:
-      weather_img.src = "img/rain.png";
+      weather_img.src = "rain.png";
       break;
     case `Mist`:
-      weather_img.src = "img/mist.png";
+      weather_img.src = "mist.png";
       break;
     case `Snow`:
-      weather_img.src = "img/snow.png";
+      weather_img.src = "snow.png";
       break;
   }
 }
